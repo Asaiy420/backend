3b5e6d6d09b6b14fe3fb1e8f9387784294f51c90
Asaiy420 <kcaryan05@gmail.com>
2025-03-24 12:58:43 +0545

    added routees, controllers and fixed some minor bugs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
 
+dotenv.config({
+  path: "./env",
+});
 const app = express()
 
 app.use(cors({
@@ -13,5 +17,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//*implementing routes
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users", userRouter)
 
 export { app }
